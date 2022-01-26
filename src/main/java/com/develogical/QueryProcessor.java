14158b352c5cf5fb7f11579dc1e79c38cbae50e3
Ali Abidi <aliabidi@Alis-MacBook-Pro.local>
2022-01-26 22:44:53 +0000

    Add query about Ihowa

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -6,12 +6,18 @@ public class QueryProcessor {
         if (query.toLowerCase().contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
-                    "writer in the English language and the world's pre-eminent dramatist.";
+                    "writer in the English language and the world's pre-eminent dramatist." +
+                    "He was born in Stratford-upon-Avon.";
         }
 
         if (query.toLowerCase().contains("imperial")) {
             return "Imperial College is a university in London";
         }
+
+        if (query.toLowerCase().contains("ihowa")) {
+            return "Ihowa is a 4th Year MEng Computing student at Imperial College London";
+        }
+
         return "";
     }
 }
